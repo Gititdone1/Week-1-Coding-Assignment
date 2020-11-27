3bdc55272b512e3d55634b06fee598341b4ec65e
Raymo Washington <raymowash100@gmail.com>
2020-11-27 15:48:28 -0700

    This is my First Assignment

---
+++ Assignment1/index.js
@@ -0,0 +1,23 @@
+var itemPrice = 20;
+
+var amountOfMoneyInWallet = 300;
+
+var numberOfFriends = 3;
+
+var ageInYears = 30;
+
+var firstName = "Raymo ";
+var lastName = " Washington";
+var middleInitial = " E ";
+
+var amountOfInWalletAfterBuyingTheItem = amountOfMoneyInWallet - itemPrice;
+var numberOfFriendsPerYear = ageInYears / numberOfFriends;
+var fullName = firstName + middleInitial + lastName;
+
+console.log("The Item Price Is: " + itemPrice);
+console.log("Amount of Money In Wallet: " + amountOfMoneyInWallet );
+console.log("My Number Of Friends: " + numberOfFriends);
+console.log("My Age:" + ageInYears);
+console.log("My First Name is: " + firstName);
+console.log("My Last Name is: " + lastName);
+console.log("My Middle Initial is:" + middleInitial)
